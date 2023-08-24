b5d303f51f32ab2e362863bd5e368648b1a322d5
DavidHTwastaken <david.houletymeczko@ontariotechu.net>
2023-08-24 19:45:09 -0400

    messing around

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
 import discord
+from dotenv import load_dotenv
+import os
+load_dotenv()
+key = os.environ.get_env('BOT_KEY')
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -7,8 +12,9 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print(f'Message from {message.author}: {message.content}')
 
+
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run('my token goes here')
+client.run(key)
