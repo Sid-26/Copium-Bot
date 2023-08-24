dabff02491ed4ff4b6610de8ad970fe32db63f5c
Sid-26 <siddhantdas262@gmail.com>
2023-08-23 21:58:46 -0400

    created hello world program

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
 import discord
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
-
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
-client.run('my token goes here')
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello! :avgcsmajor:')
+
+client.run('MTE0NDA3MjQzNTI3NzI0MjQ1MQ.GHeipf.J6O9ZDoWJH-DeQ7wB6VfXQoJThgmnM3Ibvy81o')
