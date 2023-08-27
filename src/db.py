33b0cb51ce2636a0bd79d231c37c2adbfbfc74bc
DavidHTwastaken <david.houletymeczko@ontariotechu.net>
2023-08-27 13:59:28 -0400

    Froze the requirements and added some request functions

--- src/db.py
+++ src/db.py
@@ -2,5 +2,19 @@ import requests
 
 def getAllCourses():
     url = 'https://search.ezcampus.org/searchIndex/search'
-    body = {"search_term": "ture","page": 1,"results_per_page": 14,"term_id": 1}
-    requests.post(url,)
+    # Need to think of a way to go through every single search possibility
+
+def findCourses(search_term: str, term_id: int, results_per_page: int=2**31-1, page: int=1):
+    url = 'https://search.ezcampus.org/searchIndex/search'
+    body = {"search_term": search_term,"page": page,"results_per_page": results_per_page,"term_id": term_id}
+    return requests.post(url,body).json()
+
+def listSchools():
+    url = 'https://search.ezcampus.org/searchIndex/school'
+    return requests.get(url).json()
+
+def main():
+    print(listSchools())
+
+if __name__ == '__main__':
+    main()
